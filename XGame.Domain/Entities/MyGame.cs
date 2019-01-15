52521861c39d4d4ce54dcb10bcbe1d8d4a4e17b7
Leonardo Almeida <lafraga91@gmail.com>
2019-01-15 20:18:17 -0200

    Project Creation
    I'm created project from Home and I'll want to use this code in work.

---
+++ XGame.Domain/Entities/MyGame.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace XGame.Domain.Entities
+{
+    public class MyGame
+    {
+        public Guid Id { get; set; }
+
+        public GamePlatform GamePlatform { get; set; }
+
+        public bool IsDesired { get; set; }
+
+        public DateTime DateDesired { get; set; }
+
+        public bool IsExchangeable { get; set; }
+
+        public bool OnSale { get; set; }
+    }
+}
